76c68d8e01f26865bd18a4ea1716c09d069c59bf
Lokesh Khati <lokeshkhati005@gmail.com>
2023-01-02 21:26:33 +0530

    feat : add some polyfill

---
+++ HOF/some.js
@@ -0,0 +1,20 @@
+const arr = [10, 20, 30, 40, 50, 60]
+
+// const newArr = arr.some((value, index, array) => {
+//     return (value < 40)
+// })
+
+Array.prototype.mySome = function (callback) {
+    for (let index = 0; index < this.length; index++) {
+        if (callback(this[index], index, this) !== false) {
+            return true
+        }
+    }
+    return false
+}
+
+const newArr = arr.mySome((value, index, array) => {
+    return (value < 40)
+})
+
+console.log(newArr)
