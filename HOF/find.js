6a67b77b98a1f0577faf1250b7159a664412a6e7
Lokesh Khati <lokeshkhati005@gmail.com>
2023-01-03 13:22:46 +0530

    feat : add polyfill of find

---
+++ HOF/find.js
@@ -0,0 +1,23 @@
+let arr = [1, 2, 3, 4, 5]
+
+// const found = arr.find((value) => {
+//    return  value >4
+// })
+
+
+Array.prototype.myFind = function (callback) {
+
+    for (let index = 0; index < this.length; index++) {
+        if (callback(this[index], index, this)) {
+            return this[index]
+        }
+    }
+
+    return undefined
+
+}
+
+const found = arr.myFind((value) => {
+    return value === 4
+})
+console.log(found)
