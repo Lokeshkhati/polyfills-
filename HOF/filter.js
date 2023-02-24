c86974e629738c258913d71a032f2df3d5010e97
Lokesh Khati <lokeshkhati005@gmail.com>
2023-02-24 14:52:24 +0530

    refactor : refactor filter polyfill" "

--- HOF/filter.js
+++ HOF/filter.js
@@ -1,22 +1,21 @@
 let arr = [1, 2, 3, 4, 5, 6]
 
-// let newArr = arr.filter((item) => item % 2 === 0)
+// let newArr1 = arr.filter((item) => item % 2 === 0)
 
 Array.prototype.myFilter = function (callback) {
     let returnedArray = []
     for (let index = 0; index < this.length; index++) {
-        if (callback(this[index], index, this) !== undefined) {
-            returnedArray.push(callback(this[index], index, this))
+        if (callback(this[index], index, this)) {
+            returnedArray.push(this[index])
         }
     }
     return returnedArray
 }
 
-let newArr = arr.myFilter((value, index, array) => {
-    if (value % 2 === 0) {
-        return value
-    } 
-})
 
+let newArr = arr.myFilter((value, index, array) => value%2===0)
+
+const newArr1 = arr.myFilter((value, index, array) =>value>2)
+
+console.log(newArr1)
 console.log(newArr)
-console.log(arr)
