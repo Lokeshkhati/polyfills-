010ed4c1d5dae6ce15a655b602086444b33eb096
lokesh <lokeshkhati005@gmail.com>
2023-08-01 00:27:46 +0530

    add polyfill of promise.race

--- Promise/promiseRace.js
+++ Promise/promiseRace.js
@@ -11,5 +11,26 @@ const p2 = new Promise((resolve, reject) => {
 const p3 = new Promise((resolve, reject) => {
     setTimeout(() => {
         reject('p3')
-    }, 4000)
+    }, 0)
 })
+
+
+// takes array of promises, returns a promise, 
+
+
+const promiseRace = (promises) => {
+    return new Promise((resolve, reject) => {
+        promises.forEach(async (promise) => {
+            try {
+                const value = await promise
+                resolve(value)
+            } catch (error) {
+                reject(error)
+            }
+        })
+
+    })
+}
+
+
+promiseRace([p1, p2, p3]).then((res) => console.log(res)).catch((error) => console.log(error))
